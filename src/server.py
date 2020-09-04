7f52e996f048bc53476bd2ab6ab583aaa5b61a40
vvilliam <wpalin@gmail.com>
2020-09-04 11:53:35 -0400

    feat(nginx): Add nginx to docker container to handle concurrency

--- src/server.py
+++ src/server.py
@@ -31,5 +31,5 @@ def disclosure_url():
     except:
         return {"success": False, "msg": "Complete failure"}
 
-
-app.run(host="0.0.0.0", port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
